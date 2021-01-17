239822f4f833adcecf4af041e240ea2c913fe225
Tammarra McPhaul <shaemc22@gmail.com>
2021-01-17 17:57:35 -0500

    commit tree 2b909b71f6e53ffefb37b4d8035dcca4f4b8767d

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    array.collect do |name|
+        badge_maker(name)
+    end
+end
+
+def assign_rooms(attendees)
+    new_array = []
+    attendees.each_with_index do |name, room| 
+        room += 1
+        new_array << "Hello, #{name}! You'll be assigned to room #{room}!" 
+        
+end
+new_array
+end
+
+def printer(attendees)
+     batch_badge_creator(attendees).each do |name|
+        puts name
+     end
+     
+     assign_rooms(attendees).each do |room|
+        puts room
+     end
+
+end
